1b277d6649d843af745774ee2050e79d09506822
= <dev.n.menon@gmail.com>
2021-07-30 15:50:59 -0500

    added logout

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@
   }  from "https://deno.land/x/oak@v8.0.0/mod.ts";
 
 
-  import {login,postLogin,register,postRegister,userhome,staticfiles} from "./routes.ts"; // handler functions for router stored here
+  import {login,postLogin,register,postRegister,userhome,staticfiles,logout} from "./routes.ts"; // handler functions for router stored here
 
 
 
@@ -20,6 +20,7 @@ router// directs routes to handlers
   .get("/register",register) 
   .post("/register",postRegister)
   .get("/user/:id",userhome) 
+  .get("/logout",logout)
   .get("/public/css/:path+",staticfiles)
 
 
